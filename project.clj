cb5571d9213659b46c2a5c048d695b5324cb3287
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-12-09 17:58:55 +0100

    POC

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurail "0.1.0-SNAPSHOT"
+(defproject poc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,4 +7,4 @@
                  [javax.servlet/servlet-api "2.5"]
                  [metosin/jsonista "0.3.5"]
                  [ring "1.9.4"]]
-  :repl-options {:init-ns clojurail.core})
+  :repl-options {:init-ns poc.core})
