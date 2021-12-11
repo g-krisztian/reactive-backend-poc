5e1fb9b8c1682d8055c2d8a2dfcee06804e9e0b8
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-12-11 16:40:10 +0100

    Refactor

--- src/poc/core.clj
+++ src/poc/core.clj
@@ -19,6 +19,36 @@
   (let [seq-uri (re-seq #"/[a-zA-Z0-9]*" uri)]
     (sub-router routes seq-uri)))
 
+(defn watcher-fn
+  [dependencies subscribers]
+  (fn [key watched old-state new-state]
+    (prn "watcher " key)
+    (let [oldv (get old-state key)
+          newv (get new-state key)]
+      (when-not (= oldv newv)
+        (let [actions (get subscribers key)
+              results (for [a actions]
+                        (a dependencies newv))
+              rs (into {} (for [r results]
+                            (reduce (fn [s [k v]]
+                                      (cond
+                                        (map? v) (update s k merge v)
+                                        :else (assoc s k v))) new-state r)))]
+          (reset! watched rs))))))
+
+(defn  deliver-response
+  [response-promise]
+  [:response
+   (fn [_ new-state]
+     (prn "response " new-state)
+     (deliver response-promise new-state)
+     nil)])
+
+(defn subscribe
+  [subscribers [k f]]
+  (let [subs (get subscribers k [])]
+    (assoc subscribers k (conj subs f))))
+
 (defn handler
   [dependencies routes]
   (fn [request]
@@ -27,32 +57,9 @@
                actions (router routes uri)
                ctx (atom {})
                response-promise (promise)
-               subscribers (atom {})
-               subscribe (fn [k f]
-                           (let [subs (get @subscribers k [])]
-                             (swap! subscribers assoc k (conj subs f))))
-               watcher (fn [key watched old-state new-state]
-                         (prn "watcher " key)
-                         (let [oldv (get old-state key)
-                               newv (get new-state key)]
-                           (when-not (= oldv newv)
-                             (let [actions (get @subscribers key)
-                                   results (for [a actions]
-                                             (a dependencies newv))
-                                   rs (into {} (for [r results]
-                                                 (reduce (fn [s [k v]]
-                                                           (cond
-                                                             (map? v) (update s k merge v)
-                                                             :else (assoc s k v))) new-state r)))]
-                               (reset! watched rs)))))
-               deliver-response [:response
-                                 (fn [_ new-state]
-                                   (prn "response " new-state)
-                                   (deliver response-promise new-state)
-                                   nil)]]
-           (doseq [[k f] (conj actions deliver-response)]
-             (subscribe k f)
-             (add-watch ctx k watcher))
+               subscribers (reduce subscribe {} (conj actions (deliver-response response-promise)))]
+           (doseq [[k _] subscribers]
+             (add-watch ctx k (watcher-fn dependencies subscribers)))
            (swap! ctx assoc :request request)
            (deref response-promise 1000 {:status 500
                                          :body   "request timed out"}))
